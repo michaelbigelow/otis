b8b1a606cc70ae8e4b6cae6c35140d0e6587f2d2
michaelbigelow <michaeljohnbigelow@gmail.com>
2022-03-03 20:07:04 -0700

    otis ui files add

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+
+## [1.0.0] 2022-01-12
+
+### Original Release
+
+- Added MUI as base framework
---
+++ src/index.js
@@ -0,0 +1,26 @@
+/**
+=========================================================
+* Otis Kit PRO - v2.0.0
+=========================================================
+
+* Product Page: https://material-ui.com/store/items/otis-kit-pro-material-kit-react/
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
